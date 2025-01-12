1d51b8e3e7ecf06edcd7a4f46168a1882302c4ee
Scott Murphy Heiberg <scott@alwaysvip.com>
2025-01-12 14:42:56 -0800

    Proper formatting of checkbox for bootstrap 5

--- grails-app/taglib/grails/plugin/formfields/FormFieldsTagLib.groovy
+++ grails-app/taglib/grails/plugin/formfields/FormFieldsTagLib.groovy
@@ -700,9 +700,19 @@ class FormFieldsTagLib {
 			}
 		}
 
+		boolean dateWidget = model.type in [Date, Calendar, java.sql.Date, java.sql.Time, LocalDate, LocalDateTime]
+		if (!dateWidget) {
+			attrs.remove('selectDateClass')
+		}
+		boolean checkBox = model.type in [boolean, Boolean]
+		String checkBoxClass = attrs.remove('checkBoxClass')
+		if (checkBox && checkBoxClass) {
+			attrs['class'] = checkBoxClass
+		}
+
 		if (model.type in [String, null]) {
 			return renderStringInput(model, attrs)
-		} else if (model.type in [boolean, Boolean]) {
+		} else if (checkBox) {
 			return g.checkBox(attrs)
 		} else if (model.type.isPrimitive() || model.type in Number) {
 			return renderNumericInput(propertyAccessor, model, attrs)
@@ -714,7 +724,7 @@ class FormFieldsTagLib {
 			return renderAssociationInput(model, attrs)
 		} else if (oneToMany) {
 			return renderOneToManyInput(model, attrs)
-		} else if (model.type in [Date, Calendar, java.sql.Date, java.sql.Time, LocalDate, LocalDateTime]) {
+		} else if (dateWidget) {
 			return renderDateTimeInput(model, attrs)
 		} else if (model.type in [byte[], Byte[], Blob]) {
 			return g.field(attrs + [type: "file"])
